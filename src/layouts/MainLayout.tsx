472cb9c289d75508e06bbd97ed64a56d6b9ad2c7
Heidelberg Service <batteryxv88@yandex.ru>
2025-03-16 20:41:14 +0300

    changed colors of footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,12 @@
 import Footer from '../components/Footer/Footer';
 import styles from './MainLayout.module.scss';
+import { ReactNode } from 'react';
 
-const MainLayout = ({ children }) => {
+interface MainLayoutProps {
+    children: ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
     return (
         <div className={styles.layout}>
             {/* Ваш Header */}
